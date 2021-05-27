e1cf00f5e0109e45cf0c69fc2522f2ccff502064
Jose Maria Zamora Perez <jzamora@biada.net>
2021-05-27 13:29:44 +0200

    ejercicio2.5

---
+++ js/main.js
@@ -0,0 +1,12 @@
+let italiano = "Italiani";
+let noitaliano = "Italiano";
+
+function busqueda(apellido){
+  if(apellido.charAt(apellido.length-1) === "i"){
+    console.log("Este apellido podria ser italiano");
+  }else{
+    console.log("Este apellido podria no ser italiano");
+  }
+}
+busqueda(italiano);
+busqueda(noitaliano);
